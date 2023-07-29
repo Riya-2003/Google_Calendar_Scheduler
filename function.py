cc5ebb39433b6bf7858dc2f7578c1aa7da2e0472
Riya Jain <jriya3002@gmail.com>
2023-07-29 15:26:22 +0530

    First Commit

---
+++ function.py
@@ -0,0 +1,188 @@
+import datefinder
+from api_call import service
+from datetime import datetime, timedelta
+from googleapiclient.errors import HttpError
+from api_call import service
+import datefinder
+import pytz
+
+# Function to get upcoming events with their IDs
+def get_upcoming_events(service):
+    try:
+        now = datetime.utcnow().isoformat() + "Z"
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return None
+
+        return events
+
+    except HttpError as error:
+        print("An error occurred: %s" % error)
+        return None
+
+
+def list_upcoming_events():
+    events = get_upcoming_events(service)
+    if events:
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"], event["id"])
+
+
+def delete_event(event_name):
+    events = get_upcoming_events(service)
+    if events:
+        matching_events = [event for event in events if event["summary"].lower() == event_name.lower()]
+        if matching_events:
+            print("Matching events to be deleted:")
+            for event in matching_events:
+                print(event["start"].get("dateTime", event["start"].get("date")), event["summary"], event["id"])
+
+            confirmation = input("Do you want to delete these events? (yes/no): ")
+            if confirmation.lower() == "yes":
+                for event in matching_events:
+                    event_id = event["id"]
+                    try:
+                        service.events().delete(calendarId="primary", eventId=event_id).execute()
+                        print("Event deleted successfully:", event["summary"])
+                    except HttpError as e:
+                        print(f"An error occurred while deleting the event: {e}")
+            else:
+                print("Event deletion cancelled.")
+        else:
+            print("No matching events found.")
+    else:
+        print("No upcoming events found.")
+
+
+def create_event(date, time, name, duration=1, description=None, location=None):
+    
+    start_time_str = date + " " +time
+    matches = list(datefinder.find_dates(start_time_str))
+    if len(matches):
+        start_time = matches[0]
+        end_time = start_time + timedelta(hours=duration)
+
+        event = {
+            'summary': name,
+            'location': location,
+            'description': description,
+            'start': {
+                'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                'timeZone': 'Asia/Kolkata',
+            },
+            'end': {
+                'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                'timeZone': 'Asia/Kolkata',
+            },
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10},
+                ],
+            },
+        }
+        try:
+            event_result = service.events().insert(calendarId="primary", body=event).execute()
+            print("Event created successfully!")
+            return event_result
+        except HttpError as e:
+            print(f"An error occurred while creating the event: {e}")
+            return None
+    else:
+        print("Error: Invalid format. Event not created.")
+        return None
+
+import datefinder
+from googleapiclient.errors import HttpError
+
+def update_event(event_name, **kwargs):
+    events = get_upcoming_events(service)
+    if events:
+        matching_events = [event for event in events if event["summary"].lower() == event_name.lower()]
+        if matching_events:
+            print("Matching events to be updated:")
+            for event in matching_events:
+                print(event["start"].get("dateTime", event["start"].get("date")), event["summary"], event["id"])
+            print(event["start"].get("dateTime"))
+            print(event["end"].get("dateTime"))
+            confirmation = input("Do you want to update these events? (yes/no): ")
+            if confirmation.lower() == "yes":
+                for event in matching_events:
+                    event_id = event["id"]
+                    try:
+                        updated_event = service.events().get(calendarId="primary", eventId=event_id).execute()
+                        print(event["start"].get("duration"))
+                        date = kwargs.get("date")
+                        start_hours, start_minutes = map(int, [event["start"]["dateTime"][11:13], event["start"]["dateTime"][14:16]])
+                        end_hours, end_minutes = map(int, [event["end"]["dateTime"][11:13], event["end"]["dateTime"][14:16]])
+
+                        # Calculate the time difference using timedelta
+                        duration = kwargs.get("duration")
+                        if duration:
+                            time_difference = timedelta(hours=duration)
+                        else :
+                            time_difference = timedelta(hours=end_hours - start_hours, minutes=end_minutes - start_minutes)
+                            
+                        if date:
+                            start_time_str = date + " " + updated_event["start"].get("dateTime")[11:16]  # Extracting time from dateTime
+                            matches = list(datefinder.find_dates(start_time_str))
+                            if len(matches):
+                                start_time = matches[0]
+                                end_time = start_time + time_difference
+                                updated_event['start']['dateTime'] = start_time.strftime("%Y-%m-%dT%H:%M:%S")
+                                updated_event['end']['dateTime'] = end_time.strftime("%Y-%m-%dT%H:%M:%S")
+
+                        time = kwargs.get("time")
+                        if time:
+                            start_time_str = updated_event["start"].get("dateTime")[:10] + " " + time  # Extracting date from dateTime
+                            matches = list(datefinder.find_dates(start_time_str))
+
+                            if len(matches):
+                                start_time = matches[0]
+                                updated_event['start']['dateTime'] = start_time.strftime("%Y-%m-%dT%H:%M:%S")
+                                if len(matches):
+                                    start_time = matches[0]
+                                    end_time = start_time +time_difference
+                                    updated_event['start']['dateTime'] = start_time.strftime("%Y-%m-%dT%H:%M:%S")
+                                    updated_event['end']['dateTime'] = end_time.strftime("%Y-%m-%dT%H:%M:%S")
+                        print("Start time : " + start_time_str)
+                        
+                        name = kwargs.get("name")
+                        if name:
+                            updated_event['summary'] = name
+
+                        description = kwargs.get("description")
+                        if description:
+                            updated_event['description'] = description
+
+                        location = kwargs.get("location")
+                        if location:
+                            updated_event['location'] = location
+
+                        updated_event = service.events().update(calendarId="primary", eventId=event_id, body=updated_event).execute()
+                        print("Event Updated Successfully:", updated_event["summary"])
+
+                    except HttpError as e:
+                        print(f"An error occurred while updating the event: {e}")
+            else:
+                print("Event update cancelled.")
+        else:
+            print("No matching events found.")
+    else:
+        print("No upcoming events found.")
---
+++ llm.py
@@ -0,0 +1,74 @@
+from langchain import OpenAI
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+import os
+from langchain.agents import Tool
+from function import create_event, update_event, delete_event, get_upcoming_events
+from langchain.agents import AgentType
+from langchain.agents import initialize_agent
+from typing import Optional
+from pydantic import BaseModel, Field
+from langchain.tools import StructuredTool
+
+# Load environment variables
+load_dotenv()
+
+# Initialize the language model
+llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+# Pydantic models for input parameters
+class DeleteEventParameters(BaseModel):
+    event_name: str = Field(description="name of the event to be deleted")
+
+class UpdateEventParameters(BaseModel):
+    event_name: str = Field(description="name of the event to be updated")
+    date: Optional[str] = Field(description="updated start date of the event (format: YYYY-MM-DD) (optional)")
+    time: Optional[str] = Field(description="updated start time of the event (format: HH:MM) (optional)")
+    duration: Optional[int] = Field(description="updated duration of the event in hours (optional)")
+    name: Optional[str] = Field(description="updated name or title of the event (optional)")
+    description: Optional[str] = Field(description="updated description or details of the event (optional)")
+    location: Optional[str] = Field(description="updated location of the event (optional)")
+
+class CreateEventParameters(BaseModel):
+    date: str = Field(description="start date of the event (format: YYYY-MM-DD)")
+    time: str = Field(description="start time of the event (format: HH:MM)")
+    name: str = Field(description="name or title of the event")
+    duration: int = Field(1, description="duration of the event in hours (default: 1)")
+    description: Optional[str] = Field(None, description="description or details of the event (optional)")
+    location: Optional[str] = Field(None, description="location of the event (optional)")
+
+query = input("Enter details: ")
+
+# Check if the chain was able to extract the parameters successfully
+if query and len(query) > 0:
+    
+
+    # Create a list of tools for the agent
+    llm = OpenAI(temperature=0)
+    tools = [
+        StructuredTool.from_function(
+            name="Create Event",
+            func=create_event,
+            description="Create an event in the Google calendar with the details provided",
+            args_schema=CreateEventParameters
+        ),
+        StructuredTool.from_function(
+            name="Update Event",
+            func=update_event,
+            description="Update an existing event in the Google calendar",
+            args_schema=UpdateEventParameters
+        ),
+        StructuredTool.from_function(
+            name="Delete Event",
+            func=delete_event,
+            description="Delete an event from the Google calendar",
+            args_schema=DeleteEventParameters
+        ),
+    ]
+
+    # Initialize the agent chain
+    agent_chain = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    response = agent_chain.run(input=query)
+    print(response)
+else:
+    print("Error: Unable to extract parameters from the user input.")
